4837d32e5a59e66522d7b0124695972532d610d1
Sahandgha <sahand.gha@gmail.com>
2022-01-04 10:25:10 +1000

    complete the card component and make the result section responsive

--- components/Card.js
+++ components/Card.js
@@ -4,19 +4,19 @@ import { ThumbUpIcon } from "@heroicons/react/outline";
 export default function Card({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-    <div>
+    <div className="p-3 cursor-pointer hover:text-white active:text-red-400 xl:hover:scale-105 transition-transform duration-200">
       <Image
         layout="responsive"
         src={BASE_URL + result.backdrop_path}
         width={200}
         height={100}
       />
-      <div className="">
-        <p className="truncate">{result.overview}</p>
-        <h2>{result.title || result.name}</h2>
-        <p>
+      <div className="p-2">
+        <p className="truncate text-lg">{result.overview}</p>
+        <h2 className="text-lg font-bold">{result.title || result.name}</h2>
+        <p className="flex items-center">
           {result.release_date || result.first_air_date}
-          <ThumbUpIcon className="h-5" />
+          <ThumbUpIcon className="h-5 ml-3 mr-1" />
           {result.vote_count}
         </p>
       </div>
--- components/Results.js
+++ components/Results.js
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 export default function Results({ results }) {
   return (
-    <div>
+    <div className="bg-gray-700 text-gray-200 select-none sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {results.map((result) => (
         <Card key={result.id} result={result} />
       ))}
